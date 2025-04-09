fcc2732a6fbe765a799d458da6b09797022b3df2
Mahoo12138 <mahoo12138@qq.com>
2025-04-09 13:38:55 +0800

    feat(post): 优化文章页面布局并支持 MDX 内容
    - 引入 next-mdx-remote 以支持 MDX 格式内容的渲染
    - 重构文章页面布局，优化标签和元信息的显示
    - 替换原有的 dangerouslySetInnerHTML 方式，使用 MDXRemote 组件渲染文章内容
    - 调整 PostLayout 组件，优化文章标题和元信息的样式

--- src/app/post/[...slug]/page.tsx
+++ src/app/post/[...slug]/page.tsx
@@ -2,11 +2,11 @@ import '#/styles/prism.css'
 // import 'katex/dist/katex.css'
 
 import { Metadata } from 'next'
+import { MDXRemote } from 'next-mdx-remote/rsc'
 import PostSimple from '#/layouts/PostSimple'
 import PostLayout from '#/layouts/PostLayout'
 import PostBanner from '#/layouts/PostBanner'
 import { getPost, getPosts, getPostWithPrevNext } from '#/services/post'
-
 const defaultLayout = 'PostLayout'
 
 const layouts = {
@@ -21,10 +21,10 @@ export async function generateMetadata(props: {
   return {}
 }
 
-export const generateStaticParams = async () => {
-  const posts = await getPosts()
-  return posts.map((p) => ({ slug: p.slug.split('/').map((name) => decodeURI(name)) }))
-}
+// export const generateStaticParams = async () => {
+//   const posts = await getPosts()
+//   return posts.map((p) => ({ slug: p.slug.split('/').map((name) => decodeURI(name)) }))
+// }
 
 export default async function Page(props: { params: Promise<{ slug: string[] }> }) {
   const params = await props.params
@@ -40,7 +40,8 @@ export default async function Page(props: { params: Promise<{ slug: string[] }> 
         dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
       /> */}
       <Layout post={post} next={post.next} prev={post.prev}>
-        <div dangerouslySetInnerHTML={{ __html: post.content }}></div>
+        {/* <div dangerouslySetInnerHTML={{ __html: post.content }}></div> */}
+        <MDXRemote source={post.content} />
       </Layout>
     </>
   )
--- src/layouts/PostLayout.tsx
+++ src/layouts/PostLayout.tsx
@@ -42,43 +42,41 @@ export default function PostLayout({ post, next, prev, children }: LayoutProps) 
         data-cy="postContent"
         className="bg-white p-5 pt-24 dark:border-gray-800 dark:bg-gray-800 lg:rounded-xl lg:border lg:p-20 lg:pt-20 lg:shadow-sm"
       >
-        <div className="mb-20">
-          <div className="mb-3 flex">
-            <Link href={`/category/${category.slug}`}>
-              <Label type="primary" icon="cate">
-                {category.name}
-              </Label>
-            </Link>
-          </div>
-          <h1 className="text-1.5 font-medium leading-snug tracking-wider lg:text-postTitle">
-            {title}
-          </h1>
-          <p className="mt-2 flex space-x-2 whitespace-nowrap text-5 tracking-wide text-gray-500 lg:text-xl">
-            {tags && (
-              <span className="mr-4 flex flex-wrap items-center gap-3">
-                {tags.map((tag) => (
-                  <Tag key={tag.slug} text={tag.name} />
-                ))}
-              </span>
-            )}
-            <span>
-              Posted <TimeAgo date={date} />
-            </span>
-            <span>·</span>
-            {/* <span>{post.post_metas.views} Views</span> */}
-            <span>12138 Views</span>
-
-            <span>·</span>
-            {readingTime && (
-              <span className="group cursor-pointer">
-                <span className="group-hover:hidden">{readingTime.words} Words</span>
-                <span className="hidden group-hover:block">
-                  <abbr title="Estimated reading time">ERT {readingTime.minutes} min</abbr>
-                </span>
-              </span>
-            )}
-          </p>
+        <div className="mb-3 flex">
+          <Link href={`/category/${category.slug}`}>
+            <Label type="primary" icon="cate">
+              {category.name}
+            </Label>
+          </Link>
         </div>
+        <h1 className="text-1.5 font-medium leading-snug tracking-wider lg:text-postTitle">
+          {title}
+        </h1>
+        <p className="mt-2 flex space-x-2 whitespace-nowrap text-5 tracking-wide text-gray-500 lg:text-xl">
+          {tags && (
+            <span className="mr-4 flex flex-wrap items-center gap-3">
+              {tags.map((tag) => (
+                <Tag key={tag.slug} text={tag.name} />
+              ))}
+            </span>
+          )}
+          <span>
+            Posted <TimeAgo date={date} />
+          </span>
+          <span>·</span>
+          {/* <span>{post.post_metas.views} Views</span> */}
+          <span>12138 Views</span>
+
+          <span>·</span>
+          {readingTime && (
+            <span className="group cursor-pointer">
+              <span className="group-hover:hidden">{readingTime.words} Words</span>
+              <span className="hidden group-hover:block">
+                <abbr title="Estimated reading time">ERT {readingTime.minutes} min</abbr>
+              </span>
+            </span>
+          )}
+        </p>
         <div className="divide-y divide-gray-200 dark:divide-gray-700 xl:col-span-3 xl:row-span-2 xl:pb-0">
           <div className="prose max-w-none pb-8 pt-10 dark:prose-invert">{children}</div>
         </div>
