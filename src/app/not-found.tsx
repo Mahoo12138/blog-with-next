ed7fe9660a1e3686b96fb1ddfe120dcd0ea0b797
Mahoo12138 <mahoo12138@qq.com>
2025-04-11 10:42:14 +0800

    refactor(not-found): 重构 404 页面组件
    - 更新页面布局和样式，使其更加居中和简洁
    - 添加 metadata 信息，用于设置页面标题
    - 更新文本内容和按钮样式，提高用户体验

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,24 +1,30 @@
 import { PageLayout } from '#/components/Layout'
+import Button from '#/components/ui/Button'
+
 import Link from '#/components/Link'
+
+export const metadata = {
+  title: '404 - Mahoo Blog',
+}
 function NotFound() {
   return (
-    <div className="flex flex-col items-start justify-start md:mt-24 md:flex-row md:items-center md:justify-center md:space-x-6">
-      <div className="space-x-2 pb-8 pt-6 md:space-y-5">
-        <h1 className="text-6xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 md:border-r-2 md:px-6 md:text-8xl md:leading-14">
-          404
-        </h1>
-      </div>
-      <div className="max-w-md">
-        <p className="mb-4 text-xl font-bold leading-normal md:text-2xl">
-          Sorry we couldn't find this page.
-        </p>
-        <p className="mb-8">But dont worry, you can find plenty of other things on our homepage.</p>
-        <Link
-          href="/"
-          className="effect-pressing flex w-max cursor-pointer items-center justify-center rounded-md border border-blue-500 bg-blue-500 px-7 py-2 text-xl tracking-wider text-white shadow-sm hover:border-blue-600 hover:bg-blue-600 hover:shadow-inner focus:outline-none dark:border-blue-900 dark:bg-blue-900 dark:text-gray-300 dark:hover:border-blue-800 dark:hover:bg-blue-800"
-        >
-          Back to homepage
-        </Link>
+    <div>
+      <div className="mt-0 flex h-[65vh] items-center justify-center pt-24 lg:mt-20 lg:pt-0">
+        <div className="text-center">
+          <h1 className="mb-2 text-3xl font-medium leading-14 tracking-wide text-black dark:text-white lg:text-4xl">
+            Oops
+          </h1>
+          <p className="text-3 font-light leading-14 tracking-wide text-gray-500 lg:text-2">
+            404 Not Found
+          </p>
+          <div className="mt-4 inline-block justify-center">
+            <Link href="/" className="inline-block">
+              <Button type="primary" className="mx-auto">
+                Back to Home
+              </Button>
+            </Link>
+          </div>
+        </div>
       </div>
     </div>
   )
